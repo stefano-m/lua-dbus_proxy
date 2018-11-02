b5ecf8b73b1be05a932a64ccf2decc3d1d9d2d98
Stefano Mazzucco <stefano@curso.re>
2018-11-02 21:50:03 +0000

    Update copyright header

--- src/dbus_proxy/_proxy.lua
+++ src/dbus_proxy/_proxy.lua
@@ -1,5 +1,6 @@
 --[[
   Copyright 2017 Stefano Mazzucco
+  Copyright 2018 Stefano Mazzucco and contributors
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
